ae1fc6acd16252fbe427a438f42d25a7ce59c13b
stephenolson <stephenolson@gmail.com>
2019-04-17 15:28:26 -0400

    First Commit

--- lib/number_one_songs/song.rb
+++ lib/number_one_songs/song.rb
@@ -1,22 +1,22 @@
 class NumberOneSongs::Song
 
-    attr_accessor :number, :reached_number_one, :artist, :single, :weeks_at_number_one, :reference
+  attr_accessor :number, :reached_number_one, :artist, :single, :weeks_at_number_one, :reference
   
-    @@all = []
+  @@all = []
   
-    def initialize(number=nil, reached_number_one=nil, artist=nil, single=nil, weeks_at_number_one=nil, reference=nil)
-      @number = number
-      @reached_number_one = reached_number_one
-      @artist = artist
-      @single = single
-      @weeks_at_number_one = weeks_at_number_one
-      @reference = reference
+  def initialize(number=nil, reached_number_one=nil, artist=nil, single=nil, weeks_at_number_one=nil, reference=nil)
+    @number = number
+    @reached_number_one = reached_number_one
+    @artist = artist
+    @single = single
+    @weeks_at_number_one = weeks_at_number_one
+    @reference = reference
   
-      @@all << self
-    end
-  
-    def self.all
-      @@all
-    end
-  
+    @@all << self
   end
+  
+  def self.all
+    @@all
+  end
+  
+end
